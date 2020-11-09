373c395bfc28a99e6a1a1be4780b246002a9d775
artchier <ar-chaves@hotmail.com>
2020-11-09 02:19:32 -0300

    Splash Screen

---
+++ app/src/main/java/br/com/digitalhouse/desafioandroid/SplashActivity.kt
@@ -0,0 +1,17 @@
+package br.com.digitalhouse.desafioandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }, 1000)
+    }
+}
