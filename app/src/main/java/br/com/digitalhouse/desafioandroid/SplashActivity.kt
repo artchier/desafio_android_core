7b3558fd1da19031e068bf01685fcad9e0ce9fd9
artchier <ar-chaves@hotmail.com>
2020-11-09 05:37:47 -0300

    Login Screen

--- app/src/main/java/br/com/digitalhouse/desafioandroid/SplashActivity.kt
+++ app/src/main/java/br/com/digitalhouse/desafioandroid/SplashActivity.kt
@@ -11,7 +11,8 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed(Runnable {
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }, 1000)
     }
 }
